c81aac6d7b069264aeb5ea199cb987a114428c0a
putracode <saputra219021@gmail.com>
2026-05-17 18:18:49 +0700

    Perbaikan relasi database

---
+++ backend/migrations/1779007907114_drop-category-column-from-glosarium.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.dropColumn("glosarium", "category");
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.addColumn("glosarium", {
+    category: {
+      type: "TEXT",
+    },
+  });
+};
