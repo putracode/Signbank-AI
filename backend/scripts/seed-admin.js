f7e5d1d171810eaf6fc77b06308b55e81770297f
putracode <saputra219021@gmail.com>
2026-06-03 19:39:49 +0700

    Perbaikan seedadmin

--- backend/scripts/seed-admin.js
+++ backend/scripts/seed-admin.js
@@ -6,13 +6,15 @@ import { nanoid } from 'nanoid';
 
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  : new Pool({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+    });
 
 async function seedAdmin() {
   const id = `user-${nanoid(16)}`;
